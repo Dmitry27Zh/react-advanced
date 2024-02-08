15aef40baef1aa054cdc17f67de43741ade09cd5
Dmitry <dmitry27zh@gmail.com>
2024-02-08 10:12:07 +0500

    Add search state

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { useSearchUsersQuery } from '../redux/github/github.api'
 
 const HomePage = () => {
+  const [search, setSearch] = useState('')
   const { isLoading, isError, data } = useSearchUsersQuery('vasya')
 
   return (
@@ -11,6 +13,8 @@ const HomePage = () => {
           className="border py-2 px-4 w-full h-[42px] mb-2"
           type="text"
           placeholder="Search for Github username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ipsum.
