8035a7b77e6fc227d7dcc3b2a832f420657638a9
Dmitry <dmitry27zh@gmail.com>
2024-02-08 10:00:56 +0500

    Transform response

--- src/redux/github/github.api.ts
+++ src/redux/github/github.api.ts
@@ -1,17 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { ServerResponse } from '../../models/models'
+import { IUser, ServerResponse } from '../../models/models'
 
 export const githubApi = createApi({
   reducerPath: 'github/api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com' }),
   endpoints: (build) => ({
-    searchUsers: build.query<ServerResponse, string>({
+    searchUsers: build.query<IUser[], string>({
       query: (search: string) => ({
         url: 'search/users',
         params: {
           q: search,
+          per_page: 10,
         },
       }),
+      transformResponse: (response: ServerResponse) => response.items,
     }),
   }),
 })
