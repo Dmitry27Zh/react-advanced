bb85db871745196951116c55b0359abec81219b0
Dmitry <dmitry27zh@gmail.com>
2024-02-10 15:08:43 +0500

    Create github slice

---
+++ src/redux/github/github.slice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const LS_FAVORITE_KEY = 'rfk'
+
+interface InitialState {
+  favorites: string[]
+}
+
+const initialState: InitialState = {
+  favorites: JSON.parse(localStorage.getItem(LS_FAVORITE_KEY) ?? '[]'),
+}
+
+const slice = createSlice({
+  name: 'github',
+  initialState,
+  reducers: {
+    addFavorite: (state, action) => {
+      state.favorites.push(action.payload)
+      localStorage.setItem(LS_FAVORITE_KEY, JSON.stringify(state.favorites))
+    },
+    removeFavorites: (state, action) => {
+      state.favorites = state.favorites.filter((favorite) => favorite !== action.payload)
+      localStorage.setItem(LS_FAVORITE_KEY, JSON.stringify(state.favorites))
+    },
+  },
+})
+
+export default slice
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { githubApi } from './github/github.api'
 import { setupListeners } from '@reduxjs/toolkit/query'
+import githubSlice from './github/github.slice'
 
 export const store = configureStore({
   reducer: {
     [githubApi.reducerPath]: githubApi.reducer,
+    github: githubSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(githubApi.middleware)
